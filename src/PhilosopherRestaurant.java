9a8ed1dc9b30ed41a1099fc32df688c20df4b33f
Rohit Gopal <rohitgopal@gmail.com>
2013-08-20 12:22:56 -0400

    Initializes the PhilosopherRestaurant
    Settings Enum and constructor gets created

--- src/PhilosopherRestaurant.java
+++ src/PhilosopherRestaurant.java
@@ -1,9 +1,28 @@
+import PingPongManager.PingPongPlayer;
+
 
 public class PhilosopherRestaurant {
 
-	/**
-	 * @param args
+	
+	/* 
+	 * Settings for this restaurant
+	 * NUMBER_OF_PHILOSOPHERS - the number of philosophers in this simulation
+	 * MAX_BITES_FOR_PHILOSOPHERS - how many times philosophers want to eat
+	 * 
 	 */
+	private enum Settings {
+		NUMBER_OF_PHILOSOPHERS(5),		MAX_BITES_FOR_PHILOSOPHERS(5);
+		private final int value;
+		private Settings(int value) {
+            this.value = value;
+        }
+		public int getValue() {
+            return value;
+        }
+	}
+	
+	private Thread[] philosopherThreads;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PhilosopherRestaurant thinkNEat = new PhilosopherRestaurant();
@@ -17,19 +36,26 @@ public class PhilosopherRestaurant {
 	}
 
 	private void startDinner() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Dinner is starting!\n");
 	}
 
 	private PhilosopherRestaurant() {
-		
+		philosopherThreads = new Thread[Settings.NUMBER_OF_PHILOSOPHERS.getValue()];
+		for(int i = 0 ; i < philosopherThreads.length; i++ )
+			philosopherThreads[i] = new Thread(
+					new Philosopher(i, Settings.MAX_BITES_FOR_PHILOSOPHERS.getValue()));
 	}
 	
 	
 	private class Philosopher implements Runnable{
 
-		private int number;
-		private int timesEaten;
+		private int id;
+		private int bitesLeft;
+		
+		public Philosopher(int idNumber, int maxBites){
+			this.id = idNumber;
+			this.bitesLeft = maxBites;
+		}
 
 		@Override
 		public void run() {
@@ -37,9 +63,18 @@ public class PhilosopherRestaurant {
 			
 		}
 		
+		public int getId() {
+			return id;
+		}
+		
 		@Override
 		public String toString() {
-			return "Philosopher number:" + this.number + ". Times Eaten: " + this.timesEaten;
+			return "Philosopher number:" + getId() + ". Is full:" + isFull();
+		}
+
+		private boolean isFull() {
+			return bitesLeft > 0;
+			
 		}
 		
 	}
