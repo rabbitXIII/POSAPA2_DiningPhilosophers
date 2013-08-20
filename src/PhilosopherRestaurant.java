d09fd7ba5d6ead24966fc90f02fa4d9c0a222e5a
Rohit Gopal <rohitgopal@gmail.com>
2013-08-20 11:52:29 -0400

    Created method stubs for some actions

---
+++ src/PhilosopherRestaurant.java
@@ -0,0 +1,41 @@
+
+public class PhilosopherRestaurant {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PhilosopherRestaurant thinkNEat = new PhilosopherRestaurant();
+		thinkNEat.startDinner();
+		thinkNEat.waitForPhilosophers();
+	}
+	
+	private void waitForPhilosophers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void startDinner() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private PhilosopherRestaurant() {
+		
+	}
+	
+	
+	private class Philosopher implements Runnable{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		
+		
+	}
+
+}
